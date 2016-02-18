c972b7d5550535e19b0ae48446793c4b8fcc7fb9
Christoffer Nilsson <c32@live.se>
2016-02-18 19:22:23 +0100

    Combine php files

---
+++ measurements.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Simple example of extending the SQLite3 class and changing the __construct
+ * parameters, then using the open method to initialize the DB.
+ */
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('measurements.db');
+    }
+}
+
+$db = new MyDB();
+
+header('Content-Type: text/csv; charset=utf-8');
+
+$type = $htmlspecialchars($_GET["type"]);
+
+if (!strcmp($type, "temperature") && !strcmp($type, "moisture")) {
+  exit;
+}
+
+// create a file pointer connected to the output stream
+$output = fopen('php://output', 'w');
+$query = "SELECT * FROM measurements WHERE date > '" . date("Y-m-d H:i:s", strtotime('-3 days', time())) . "'";
+$result = $db->query($query);
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+  fwrite($output, $row["date"] . ";" . $row[$type] . "\n");
+}
+fclose();
+?>
